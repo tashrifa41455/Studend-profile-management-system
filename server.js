732bf8e089bdc850ce8ebcb22df78dab64277147
Tashrifa Naz <angeltashrifa07@gmail.com>
2026-06-23 16:39:05 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./backend/config/db');
 
 dotenv.config();
 connectDB();
